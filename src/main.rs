08a962ce6c20e2e28fa8c76239dcd204d2ab6239
Brandon Grooters <brandon.grooters@gmail.com>
2024-09-22 19:10:49 -0500

    altitude text

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::color::palettes::css::GREEN;
 
 const SHIP_START_Y: f32 = 0.0;
 const SHIP_START_X: f32 = 0.0;
@@ -9,7 +10,7 @@ const LANDER_MASS: f32 = 15_000.0;
 // primary fuel mass for main engine
 const LANDER_FUEL_MASS: f32 = 5_000.0;
 // main engine maximum thrust in newtons
-const LANDER_THRUST_MAX: f32 = 33_750.0;
+const LANDER_THRUST_MAX: f32 = 31_000.0;
 // main engine minimum thrust in newtons
 const LANDER_THRUST_MIN: f32 = 4_500.0;
 // gravity in m/s2
@@ -19,27 +20,58 @@ const IO_GRAVITY: f32 = 1.796;
 const CALLISTO_GRAVITY: f32 = 1.235;
 const GANYMEDE_GRAVITY: f32 = 1.428;
 
+const DEFAULT_START_ALTITUDE: f32 = 2000.0;
+
 const RCS_STEP: f32 = 0.523598775598299;
+
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, (apply_thrust, apply_velocity))
+        .add_systems(FixedUpdate, (apply_thrust, apply_velocity, update_instrument_system))
+
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>,
 mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,) {
-
+    let font = asset_server.load("fonts/spacemono-regular.ttf");
     let texture = asset_server.load("lander.png");
-    let texture_layout = TextureAtlasLayout::from_grid(Vec2{x: 64.0, y: 60.0}, 6,1, None, None);
+    let texture_layout = TextureAtlasLayout::from_grid(UVec2{x: 64, y: 60}, 6,1, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(texture_layout);
     // Camera
     commands.spawn(Camera2dBundle::default());
-    // ship
+    // display
     commands.spawn((
+        TextBundle::from_sections([
+            TextSection::new(
+                "ALTITUDE: ",
+                TextStyle {
+                    font: font.clone(),
+                    font_size: 20.0,
+                    ..default()
+                },
+            ),
+            TextSection::from_style(
+                TextStyle {
+                    font,
+                    font_size: 20.0,
+                    color: GREEN.into(),
+                }
+            ),
+        ])
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(10.0),
+            left: Val::Px(15.0),
+            ..default()
+        }),
+        LanderAltitude(DEFAULT_START_ALTITUDE)
+        ),
+    );
+    // ship
+    commands.spawn((        
         SpriteBundle {
             transform: Transform {
                 rotation: Quat::from_rotation_z((30.0_f32).to_radians()),
@@ -54,7 +86,7 @@ mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,) {
             index: 0,
 
         },
-        Ship,
+        LanderAltitude(DEFAULT_START_ALTITUDE),
         Rcs(Thruster::None),
         Velocity(Vec3::new(0.0, 0.0, 0.0)),
     ));
@@ -92,26 +124,60 @@ fn apply_thrust(
 fn apply_velocity(
     input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
-    mut query: Query<(&mut Transform, &mut Velocity)>,
+    mut query: Query<(Entity, Option<&mut Transform>, Option<&mut Velocity>, &mut LanderAltitude)>,
 ) {
-    let mut ship_velocity = query.single_mut();
-    let mut delta_velocity = Vec3::new(0.0, 0.0, 0.0);
+    let mut idx = 0;
+    for e in &query {
+        println!("Entity {}", idx);
+        idx += 1;
+    }
+    let mut text_altitude: Option<f32> = None;
+    let mut hud: Option<Entity> = None;
+    for entity in  &mut query {
+        if let Some(mut lander_velocity) = entity.2 {
+            let mut transform = entity.1.expect("all components have a transform");
+            let mut lander_altitude = entity.3;
+            let mut delta_velocity = Vec3::new(0.0, 0.0, 0.0);
+            // velocity should also be present in this case
+            // caclulate acceleration due to gravity
+            let gravity_accel = LUNAR_GRAVITY * 10.0;
+            if input.pressed(KeyCode::ArrowUp) { 
+                delta_velocity.y += 100.0;
+            } 
+            // muliply the delta velocity by Z rotation to get delta velocity based on
+            // off-axis thrust
+            delta_velocity = transform.rotation.mul_vec3(delta_velocity);
+            // gravity
+            delta_velocity.y -= gravity_accel;            
+            lander_velocity.0 += delta_velocity * time.delta_seconds();
+            transform.translation.x += lander_velocity.0.x * time.delta_seconds();
+            transform.translation.y += lander_velocity.0.y * time.delta_seconds();
+            lander_altitude.0 = DEFAULT_START_ALTITUDE + transform.translation.y;            
+            text_altitude = Some(lander_altitude.0);
+        } else {
+            hud = Some(entity.0);
+        }          
+    }
+    // get the hud and lander, set hud altitude
+    if let Some(hud) = hud {
+        if let Some(text_altitude) = text_altitude {
+            // get the hud and lander
+            let mut hud = query.get_mut(hud).expect("hud despawned");
+            // let altitude = lander.3.0;
+            hud.3.0 = text_altitude;
+        }
+    }
     
-    // caclulate acceleration due to gravity
-    let gravity_accel = LUNAR_GRAVITY * 10.0;
-    if input.pressed(KeyCode::ArrowUp) {
-        delta_velocity.y += 100.0;
-    } 
-    // muliply the delta velocity by Z rotation to get delta velocity based on
-    // off-axis thrust
-    delta_velocity = ship_velocity.0.rotation.mul_vec3(delta_velocity);
-    // gravity
-    delta_velocity.y -= gravity_accel;
-     
-    ship_velocity.1 .0 += delta_velocity * time.delta_seconds();
 
-    ship_velocity.0.translation.x += ship_velocity.1 .0.x * time.delta_seconds();
-    ship_velocity.0.translation.y += ship_velocity.1 .0.y * time.delta_seconds();
+}
+
+fn update_instrument_system(
+    time: Res<Time>,
+    mut query: Query<(&mut Text, &mut LanderAltitude)>,
+) {
+    let text_altitude = query.single_mut();
+    let mut text = text_altitude.0;
+    text.sections[1].value = format!("{}", text_altitude.1.0);
 }
 
 
@@ -121,10 +187,11 @@ enum Thruster {
     Right,
 }
 
-#[derive(Component)]
-struct Ship;
 
-#[derive(Component)]
+#[derive(Component, Debug)]
+struct LanderAltitude(f32);
+
+#[derive(Component, Debug)]
 struct Velocity(Vec3);
 
 #[derive(Component)]
